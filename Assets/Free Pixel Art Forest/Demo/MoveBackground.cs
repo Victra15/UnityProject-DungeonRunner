955a16fb778ecb634cee29505ef30d178102d9a4
com2s <haka0119@naver.com>
2021-05-23 18:26:57 +0900

    중간1

--- Assets/Free Pixel Art Forest/Demo/MoveBackground.cs
+++ Assets/Free Pixel Art Forest/Demo/MoveBackground.cs
@@ -2,41 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveBackground : MonoBehaviour {
+public class MoveBackground : MonoBehaviour
+{
 
 
-	
+
 	public float speed;
 	private float x;
 	public float PontoDeDestino;
 	public float PontoOriginal;
-
-
+	private float t;
+	public float speed_obj;
+	private float startTime;
+	private bool ismove = false;
 
 
 	// Use this for initialization
-	void Start () {
+	public void MoveAll()
+	{
 		//PontoOriginal = transform.position.x;
+		Debug.Log("start");
+		ismove = true;
+		startTime = Time.time;
+		t = startTime;
+		speed_obj = speed;
+		
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		
-		
-		x = transform.position.x;
-		x += speed * Time.deltaTime;
-		transform.position = new Vector3 (x, transform.position.y, transform.position.z);
+	void Update()
+	{
+		if (ismove)
+		{
+			Debug.Log(t);
+			x = transform.position.x;
+			x += speed_obj * Time.deltaTime;
+			transform.position = new Vector3(x, transform.position.y, transform.position.z);
+			t = Time.time - startTime;
+
+			if (t >= 3) //이동시간 3초
+			{
+				speed_obj = 0;
+				ismove = false;
+			}
+
+			if (x <= PontoDeDestino)
+			{
+
+				Debug.Log("hhhh");
+				x = PontoOriginal;
+				transform.position = new Vector3(x, transform.position.y, transform.position.z);
+
+			}
 
 
-
-		if (x <= PontoDeDestino){
-
-			Debug.Log ("hhhh");
-			x = PontoOriginal;
-			transform.position = new Vector3 (x, transform.position.y, transform.position.z);
-			
 		}
-
-
 	}
 }
